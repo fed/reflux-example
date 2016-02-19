ac67867e787fecfde243abd5a96b34a5c198d967
Federico Knüssel <fknussel@gmail.com>
2016-02-20 10:47:16 +1100

    Add views and upvotes details on hover

--- src/js/components/image.jsx
+++ src/js/components/image.jsx
@@ -14,6 +14,7 @@ module.exports = React.createClass({
                 onMouseLeave={this.handleOnMouseLeave}>
       {this.state.hover && this.props.animated ? this.renderVideo() : this.renderImage()}
       {!this.state.hover && this.props.animated ? this.renderPlayButton() : null}
+      {this.state.hover ? this.renderDetails() : null}
     </div>
   },
 
@@ -28,6 +29,13 @@ module.exports = React.createClass({
   renderPlayButton: function () {
     return <span className="gallery__image__icon-play">
       <img src="assets/play.svg" />
+    </span>
+  },
+
+  renderDetails: function () {
+    return <span className="gallery__image__details">
+      Views: {this.props.views}<br />
+      Upvotes: {this.props.ups}
     </span>
   },
 
