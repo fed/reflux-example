9cc70a9709e9b8217c03679142dcb6dc1db6c0c7
Federico Knüssel <fknussel@gmail.com>
2016-10-22 19:51:33 +1100

    Add missing dependency

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,4 +1,7 @@
+// Get your key here: https://imgur.com/account/settings/apps
+// It's the "Client Secret" value
+
 module.exports = {
   ROOT_URL: 'https://api.imgur.com/3/',
-  API_KEY: 'getyourownapikeymate'
+  API_KEY: 'getyourownkeymate'
 }
